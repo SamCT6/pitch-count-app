2b9b355e3e0116507728787311464bb8745484b6
Annabelx61 <MehariAA@hendrix.edu>
2024-10-01 22:23:57 -0500

    Did a unit test for the new code

--- to_dont_list/test/widget_test.dart
+++ to_dont_list/test/widget_test.dart
@@ -1,90 +1,33 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:to_dont_list/main.dart';
+
 import 'package:to_dont_list/objects/Grocery.dart';
 import 'package:to_dont_list/widgets/to_do_grocery.dart';
 
 void main() {
-  test('Item abbreviation should be first letter', () {
-    const item = Item(name: "add more todos");
-    expect(item.abbrev(), "a");
+  test('The first Grocery should should show 1 in circle avatar', () {
+    Grocery grocery = Grocery(name: 'eggs', price: 10.99);
+    expect(grocery.count, 1);
   });
 
-  // Yes, you really need the MaterialApp and Scaffold
-  testWidgets('ToDoListItem has a text', (tester) async {
+  testWidgets('ToDoListGrocery has a text', (tester) async {
     await tester.pumpWidget(MaterialApp(
         home: Scaffold(
-            body: ToDoListItem(
-                item: const Item(name: "test"),
+            body: ToDoListGrocery(
+                grocery: Grocery(name: "test", price: 10.99),
                 completed: true,
-                onListChanged: (Item item, bool completed) {},
-                onDeleteItem: (Item item) {}))));
+                onListChanged: (Grocery grocery, bool completed) {},
+                onDeleteGrocery: (Grocery grocery) {}))));
     final textFinder = find.text('test');
-
-    // Use the `findsOneWidget` matcher provided by flutter_test to verify
-    // that the Text widgets appear exactly once in the widget tree.
     expect(textFinder, findsOneWidget);
   });
 
-  testWidgets('ToDoListItem has a Circle Avatar with abbreviation',
-      (tester) async {
-    await tester.pumpWidget(MaterialApp(
-        home: Scaffold(
-            body: ToDoListItem(
-                item: const Item(name: "test"),
-                completed: true,
-                onListChanged: (Item item, bool completed) {},
-                onDeleteItem: (Item item) {}))));
-    final abbvFinder = find.text('t');
-    final avatarFinder = find.byType(CircleAvatar);
-
-    CircleAvatar circ = tester.firstWidget(avatarFinder);
-    Text ctext = circ.child as Text;
-
-    // Use the `findsOneWidget` matcher provided by flutter_test to verify
-    // that the Text widgets appear exactly once in the widget tree.
-    expect(abbvFinder, findsOneWidget);
-    expect(circ.backgroundColor, Colors.black54);
-    expect(ctext.data, "t");
-  });
-
-  testWidgets('Default ToDoList has one item', (tester) async {
+  testWidgets('Default ToDoList should not expect anything on display', (tester) async {
     await tester.pumpWidget(const MaterialApp(home: ToDoList()));
 
-    final listItemFinder = find.byType(ToDoListItem);
+    final listGroceryFinder = find.byType(ToDoListGrocery);
 
-    expect(listItemFinder, findsOneWidget);
+    expect(listGroceryFinder, findsNothing);
   });
-
-  testWidgets('Clicking and Typing adds item to ToDoList', (tester) async {
-    await tester.pumpWidget(const MaterialApp(home: ToDoList()));
-
-    expect(find.byType(TextField), findsNothing);
-
-    await tester.tap(find.byType(FloatingActionButton));
-    await tester.pump(); // Pump after every action to rebuild the widgets
-    expect(find.text("hi"), findsNothing);
-
-    await tester.enterText(find.byType(TextField), 'hi');
-    await tester.pump();
-    expect(find.text("hi"), findsOneWidget);
-
-    await tester.tap(find.byKey(const Key("OKButton")));
-    await tester.pump();
-    expect(find.text("hi"), findsOneWidget);
-
-    final listItemFinder = find.byType(ToDoListItem);
-
-    expect(listItemFinder, findsNWidgets(2));
-  });
-
-  // One to test the tap and press actions on the items?
 }
